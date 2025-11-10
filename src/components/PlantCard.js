55f87ea38fb63ebfa6a0256c3caf6e7c19137f2a
faith-kerry <kerubofaith944@gmail.com>
2025-11-10 21:41:21 +0300

    Worked on the PlantCard.js file

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ name, image, price }) {
+  // Local state to track sold out status
+  const [soldOut, setSoldOut] = useState(false);
+
+  function handleToggleSoldOut() {
+    setSoldOut(!soldOut);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: ${price}</p>
+      {soldOut ? (
+        <button className="primary" onClick={handleToggleSoldOut}>
+          Sold Out
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleToggleSoldOut}>In Stock</button>
       )}
     </li>
   );
